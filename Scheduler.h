bd8e5dde6c04ea69a27b4415be9c5f903f715ac2
RalphM-02 <ralph_marquez@dlsu.edu.ph>
2024-07-02 14:59:25 +0800

    feat: added variables and function for scheduler (not functional yet)

--- Scheduler.h
+++ Scheduler.h
@@ -2,12 +2,42 @@
 
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
+#include <string>
+#include <memory>
+#include <queue>
+#include <vector>
 
+#include "Cpu.h"
+#include "Process.h"
+
+using namespace std;
 class Scheduler {
 public:
-    Scheduler();
-    ~Scheduler();
     void initialize();
+    void start();
+    void stop();
+private:
+    Scheduler();
+    ~Scheduler() = default;
+
+    void run();
+
+    static Scheduler* _ptr;
+
+    int _numCpu;
+    string _scheduler;
+    int _quantumCycle;
+    bool _preemptive;
+    int _batchProcessFreq;
+    int _minIns;
+    int _maxIns;
+    int _delaysPerExec;
+    bool running = false;
+
+    queue<shared_ptr<Process>> _readyQueue;
+    vector<shared_ptr<Cpu>> _cpuList;
+    vector<shared_ptr<Process>> _processList;
+
 };
 
 #endif // SCHEDULER_H
